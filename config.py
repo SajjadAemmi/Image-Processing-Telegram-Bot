022902ebd56f0645974d1d25921fcaa6e620e6b4
SajjadAemmi <sajjadaemmi@gmail.com>
2021-09-28 21:22:02 +0330

    fix typo

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ start_test = "Hi, welcome to image processing bot. \
 
 help_text = "I can help you create beautiful images. \
              \nYou can control me by sending these commands: \
-             \n/pencil_sketch - convert yout image to pencil sketch \
-             \n/gray - convert yout color image to grayscale \
+             \n/pencil_sketch - convert your image to pencil sketch \
+             \n/gray - convert your color image to grayscale \
              \n/qrcode - create your own qrcode \
              \n/help - show this help message"
